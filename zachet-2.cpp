61a1d7f8b7806c779f70ff879bd68ca6c5782de0
alenahalm <alenahalmatova749@gmail.com>
2020-12-26 10:07:29 +0800

    Initial commit

---
+++ zachet-2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int n = 5;
+	int a[n];
+	cout << "Enter numbers" << endl;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	if (a[0] > a[1]) {
+		cout << 0 << " ";
+	}
+	for (int i = 1; i < n - 1; i++) {
+		if (a[i] > a[i - 1] && a[i] > a[i + 1]) {
+			cout << i << " ";
+		}
+	}
+	if (a[n - 1] > a[n - 2]) {
+		cout << n - 1 << " ";
+	}
+}
